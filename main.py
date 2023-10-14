4b97b571774d557b3ba3773fc3d9beac9da8d25f
rhenanbartels <rhenan.bartels@gmail.com>
2023-10-14 11:22:23 -0300

    Refactor plotting methods

--- main.py
+++ main.py
@@ -20,13 +20,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.menu_file_open_action.setShortcut("Ctrl+O")
         self.menu_file_open_action.triggered.connect(self.open_file)
 
+        # Combo boxes
+        self.topAxesComboBox.currentIndexChanged.connect(self.change_top_plot)
+        self.bottomAxesComboBox.currentIndexChanged.connect(self.change_bottom_plot)
+
         # Init last opened directory
         self.last_dir = "."
         self.file_name = ""
 
         # Init plot config variables
-        self.top_plot_roi_coord = None
-        self.bottom_plot_roi_coord = None
+        self.top_roi = None
+        self.bottom_roi = None
+        self._roi_region = None
 
     @property
     def duration(self):
@@ -54,6 +59,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def hf_range(self):
         return float(self.lineEditHFLower.text()), float(self.lineEditHFUpper.text())
 
+    @property
+    def roi_region(self):
+        if self._roi_region is None:
+            return [0, self.duration]
+        else:
+            return self._roi_region
+
     def _save_last_dir(self, file_name):
         self.last_dir = os.path.dirname(file_name)
 
@@ -61,9 +73,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         file_name = os.path.basename(file_path)
         self.lineEditFileName.setText(file_name)
 
-    def _update_edit_time_ranges(self):
-        self.lineEditStartTimeAxes.setText(f"{self.top_plot_roi_coord[0]:.2f}")
-        self.lineEditEndTimeAxes.setText(f"{self.top_plot_roi_coord[1]:.2f}")
+    def _update_edit_time_ranges(self, region):
+        self.lineEditStartTimeAxes.setText(f"{region[0]:.2f}")
+        self.lineEditEndTimeAxes.setText(f"{region[1]:.2f}")
 
     def open_file(self):
         self.file_path, _ = QFileDialog.getOpenFileName(
@@ -78,99 +90,105 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.time, self.abp, self.cbfv = open_data_frame(self.file_path)
             self._set_file_name(self.file_path)
 
-            self.plot(self.time, self.cbfv, self.abp)
-            self.top_plot_roi_coord = (0, self.duration)
-            self.bottom_plot_roi_coord = (0, self.duration)
-            self._update_edit_time_ranges()
+            self.plot_cbfv()
+            self.plot_abp()
+            self._update_edit_time_ranges(region=(0, self.duration))
 
             # Signal Processing
             # TODO: get config from panel
             fs = 10.0
             self.results = tfa(self.abp, self.cbfv, fs)
 
-            self.plot_psd(
-                self.bottom_plot,
-                self.results["frequency"],
-                self.results["pxx"],
-                vlf=self.vlf_range,
-                lf=self.lf_range,
-                hf=self.hf_range,
-            )
+    def change_top_plot(self):
+        {
+            0: self.plot_cbfv,
+            1: self.plot_cbfv_psd,
+        }.get(self.topAxesComboBox.currentIndex(), lambda: None)()
 
-    def _plot(self, time, cbv, abp):
-        global p1, p2
-        pw = self.top_plot
-        pw.setWindowTitle('pyqtgraph example: MultiplePlotAxes')
-        p1 = pw.plotItem
-        p1.setLabels(left='axis 1')
+    def change_bottom_plot(self):
+        {
+            0: self.plot_abp,
+            1: self.plot_abp_psd,
+        }.get(self.bottomAxesComboBox.currentIndex(), lambda: None)()
 
-        p2 = pg.ViewBox()
-        p1.showAxis('right')
-        p1.scene().addItem(p2)
-        p1.getAxis('right').linkToView(p2)
-        p2.setXLink(p1)
-        p1.getAxis('right').setLabel('axis2', color='#0000ff')
-
-        ## Handle view resizing
-        def updateViews():
-            ## view has resized; update auxiliary views to match
-            global p1, p2
-            p2.setGeometry(p1.vb.sceneBoundingRect())
-            ## need to re-update linked axes since this was called
-            ## incorrectly while views had different shapes.
-            ## (probably this should be handled in ViewBox.resizeEvent)
-            p2.linkedViewChanged(p1.vb, p2.XAxis)
-
-        updateViews()
-        p1.vb.sigResized.connect(updateViews)
-
-        p1.plot(time, cbv)
-        p2.addItem(pg.PlotCurveItem(time, abp, pen='b'))
-
-    def update_top_plot(self):
+    def update_top_roi(self):
         # Do not let area outside signal
-        self.top_plot_roi_coord = self.lb.getRegion()
-        self.lr.setRegion(self.top_plot_roi_coord)
-        self._update_edit_time_ranges()
+        region = self.top_roi.getRegion()
+        self._roi_region = region
+        if self.bottom_roi is not None:
+            self.bottom_roi.setRegion(region)
 
-    def update_bottom_plot(self):
+        self._update_edit_time_ranges(region)
+
+    def update_bottom_roi(self):
         # Do not let area outside signal
-        self.bottom_plot_roi_coord = self.lr.getRegion()
-        self.lb.setRegion(self.bottom_plot_roi_coord)
-        self._update_edit_time_ranges()
+        region = self.bottom_roi.getRegion()
+        self._roi_region = region
+        if self.top_roi is not None:
+            self.top_roi.setRegion(region)
 
-    def plot(self, time, cbfv, abp):
-        self.top_plot.plot(time, cbfv, pen=pg.mkPen("g",  width=2))
-        self.top_plot.setLabel("left", "CBFV (cm/s)")
-        self.top_plot.setLabel("bottom", "Time (s)")
-        self.top_plot.showGrid(x=True, y=True, alpha=1.0)
-        #self.top_plot.addItem(pg.LineROI([0,  90], [300, 0], width=1, pen=(1,9)))
+        self._update_edit_time_ranges(region)
 
-        self.lr = pg.LinearRegionItem([0, self.duration], pen=pg.mkPen(width=3.5))
-        self.lr.setZValue(-10)
+    def add_roi(self, axes, action):
+        roi = pg.LinearRegionItem(self.roi_region, pen=pg.mkPen(width=3.5))
+        roi.setZValue(-10)
 
         # Callback when area change.
-        self.lr.sigRegionChanged.connect(self.update_bottom_plot)
-        self.top_plot.addItem(self.lr)
+        roi.sigRegionChanged.connect(action)
+        axes.addItem(roi)
+        return roi
 
-        #self.top_plot.addLine(x=0, y=90, pen=pg.mkPen("b", width=3))
-        #self.top_plot.addLine(x=300, y=90, pen=pg.mkPen("b", width=3))
+    def plot_cbfv(self):
+        self.plot_time_series(
+            self.top_plot,
+            self.time,
+            self.cbfv,
+            xlabel="Time (s)",
+            ylabel="CBFV (cm/s)",
+            xlim=[0, self.duration],
+            color="g"
+        )
+        self.top_roi = self.add_roi(self.top_plot, self.update_top_roi)
 
-        self.top_plot.setRange(xRange=[self.start_signal, self.duration])
+    def plot_cbfv_psd(self):
+        self.plot_psd(
+            self.top_plot,
+            self.results["frequency"],
+            self.results["pyy"],  # TODO: change pyy to cbfv_psd
+            vlf=self.vlf_range,
+            lf=self.lf_range,
+            hf=self.hf_range,
+        )
 
-        self.bottom_plot.plot(time, abp, pen=pg.mkPen("y", width=2))
-        self.bottom_plot.setLabel("left", "ABP (mmHg)")
-        self.bottom_plot.setLabel("bottom", "Time (s)")
-        self.bottom_plot.showGrid(x=True, y=True, alpha=1.0)
-        self.lb = pg.LinearRegionItem([0, self.duration], pen=pg.mkPen(width=3.5))
-        self.lb.setZValue(-10)
+    def plot_abp(self):
+        self.plot_time_series(
+            self.bottom_plot,
+            self.time,
+            self.abp,
+            xlabel="Time (s)",
+            ylabel="ABP (mmHg)",
+            xlim=[0, self.duration],
+            color="y"
+        )
+        self.bottom_roi = self.add_roi(self.bottom_plot, self.update_bottom_roi)
 
-        # Callback when area change.
-        self.lb.sigRegionChanged.connect(self.update_top_plot)
-        self.bottom_plot.addItem(self.lb)
+    def plot_abp_psd(self):
+        self.plot_psd(
+            self.bottom_plot,
+            self.results["frequency"],
+            self.results["pxx"],  # TODO: change pxx to abp_psd
+            vlf=self.vlf_range,
+            lf=self.lf_range,
+            hf=self.hf_range,
+        )
 
-        # self.side_top_plot.plot(x, y)
-        # self.side_bottom_plot.plot(x, y)
+    def plot_time_series(self, axes, time, signal, xlabel, ylabel, xlim, color):
+        axes.clear()
+        axes.plot(time, signal, pen=pg.mkPen(color,  width=2))
+        axes.setLabel("left", ylabel)
+        axes.setLabel("bottom", xlabel)
+        axes.showGrid(x=True, y=True, alpha=1.0)
+        axes.setRange(xRange=xlim)
 
     def plot_psd(self, axes, frequency, psd, vlf, lf, hf):
         # For aesthetic reasons, the psd plot starts at 0 Hz and goes 1 frequency beyond its boundary
@@ -193,6 +211,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         axes.setRange(xRange=[0, 0.5])
         axes.setLabel("left", "PSD (ms²/Hz)")
         axes.setLabel("bottom", "Frequency (Hz)")
+        axes.showGrid(x=True, y=True, alpha=1.0)
         # axes.removeItem(self.lb)
 
 
